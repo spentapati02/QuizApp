0f423138463685ec2ca8156bed8efa1a27d6eaec
spentapati02 <113950010+spentapati02@users.noreply.github.com>
2022-10-31 18:50:52 -0500

    Update quiz2_questions.js

--- quiz2_questions.js
+++ quiz2_questions.js
@@ -1,2 +1,122 @@
-
-{"response_code":0,"results":[{"category":"Earth, Sun, and Moon Trivia","type":"multiple","difficulty":"medium","question":"How long does it take sunlight to reach the earth?","correct_answer":"8 min","incorrect_answers":["1 min","36 min","15 sec"]},{"category":"Earth, Sun, and Moon Trivia","type":"multiple","difficulty":"medium","question":"How fast does the earth move in its orbit about the sun?","correct_answer":"18 miles per sec","incorrect_answers":["18 miles per min","18 miles per hour","18 miles per day"]},{"category":"Earth, Sun, and Moon Trivia","type":"multiple","difficulty":"medium","question":"The moon’s density is what percentage of the earth’s?","correct_answer":"60%","incorrect_answers":["30%","90%","1%"]},{"category":"Earth, Sun, and Moon Trivia","type":"multiple","difficulty":"medium","question":"What region of the Earth experiences the least seasonal variation?","correct_answer":"The equatorial region","incorrect_answers":["North of the equator","South of the equator"]},{"category":"Earth, Sun, and Moon Trivia","type":"multiple","difficulty":"medium","question":"What is the largest living organism visible from the earth’s orbit?","correct_answer":"The Great Barrier Reef","incorrect_answers":["Mount Everest","The Great Lakes"]},{"category":"Earth, Sun, and Moon Trivia","type":"multiple","difficulty":"medium","question":"What Percentage of Earth’s surface is land?","correct_answer":"About 30%","incorrect_answers":["About 70%","About 10%","About 50%"]},{"category":"Earth, Sun, and Moon Trivia","type":"multiple","difficulty":"medium","question":"Which planet other than Earth was the first to be studied by a satellite launched from Earth?","correct_answer":"Venus","incorrect_answers":["Mercury","Mars","Jupiter"]},{"category":"Earth, Sun, and Moon Trivia","type":"multiple","difficulty":"medium","question":"What is the average distance from Earth to the moon?","correct_answer":"239,000 miles","incorrect_answers":["10 miles","597,000 miles","143,213 miles"]},{"category":"Earth, Sun, and Moon Trivia","type":"multiple","difficulty":"medium","question":"How long does it take for radio signals to travel from Earth to the Moon (and vice versa)?","correct_answer":"1.28 sec","incorrect_answers":["50.43 sec","1 min","20.56 sec"]},{"category":"Earth, Sun, and Moon Trivia","type":"multiple","difficulty":"medium","question":"What is the brightest star in the sky (excluding our sun and novas)?","correct_answer":"Sirius","incorrect_answers":["Vega","Canopus"]}]}
+{
+"response_code": 0,
+"results": [
+{
+"category": "Tennis",
+"type": "multiple",
+"difficulty": "medium",
+"question": "Which of the following is the highest governing body of lawn tennis?",
+"correct_answer": "ITF: International Tennis Federation",
+"incorrect_answers": [
+"WTF: World Tennis Federation",
+"WTA: World Tennis Authority",
+"TAW: Tennis Association of the World"
+]
+},
+{
+"category": "Tennis",
+"type": "multiple",
+"difficulty": "medium",
+"question": "What is the width of the Tennis Court?",
+"correct_answer": "36feet",
+"incorrect_answers": [
+"30feet",
+"28feet",
+"18feet"
+]
+},
+{
+"category": "Tennis",
+"type": "multiple",
+"difficulty": "medium",
+"question": "What is height of the Net in the centre of the court??",
+"correct_answer": "3feet",
+"incorrect_answers": [
+"2feet",
+"5feet",
+"6feet"
+]
+},
+{
+"category": "Tennis",
+"type": "multiple",
+"difficulty": "medium",
+"question": "The first U.S. Championship was organized in..........",
+"correct_answer": "1968",
+"incorrect_answers": [
+"1970",
+"1980"
+]
+},
+{
+"category": "Tennis",
+"type": "multiple",
+"difficulty": "medium",
+"question": "Which of the following are the Grand Slams held every year?",
+"correct_answer": "Australian, French, Wimbledon, US Open.",
+"incorrect_answers": [
+"Swedish, French, US Open, Italian",
+"Asia Open"
+]
+},
+{
+"category": "Tennis",
+"type": "multiple",
+"difficulty": "medium",
+"question": " In the Lawn Tennis while serving the serve always has to fall in the.............?",
+"correct_answer": "Opposite box",
+"incorrect_answers": [
+"About 70%",
+"About 10%",
+"About 50%"
+]
+},
+{
+"category": "Tennis",
+"type": "multiple",
+"difficulty": "medium",
+"question": "What is Size of Double’s Court??",
+"correct_answer": "78x36",
+"incorrect_answers": [
+"Mercury",
+"Mars",
+"Jupiter"
+]
+},
+{
+"category": "Tennis",
+"type": "multiple",
+"difficulty": "medium",
+"question": "What is length of the Tennis Court??",
+"correct_answer": "78",
+"incorrect_answers": [
+"10 miles",
+"597,000 miles",
+"143,213 miles"
+]
+},
+{
+"category": "Tennis",
+"type": "multiple",
+"difficulty": "medium",
+"question": "What is size of Single’s Court??",
+"correct_answer": "78x27",
+"incorrect_answers": [
+"50.43 sec",
+"1 min",
+"20.56 sec"
+]
+},
+{
+"category": "Tennis",
+"type": "multiple",
+"difficulty": "medium",
+"question": "The first French open was organized in??",
+"correct_answer": "1871",
+"incorrect_answers": [
+"Vega",
+"Canopus"
+]
+}
+]
+}
